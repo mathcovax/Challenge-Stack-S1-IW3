cbe30bd63377b25d5402b703790663671dac8758
mathcovax <campani.mathieu@gmail.com>
2023-02-09 18:26:37 +0100

    add tab

--- src/components/ico.js
+++ src/components/ico.js
@@ -5,6 +5,10 @@ const ico = new Component("ico");
 ico.inner(html);
 ico.props("size", "24px");
 
+ico.method("clicked", function(e){
+	this.emit("click", e);
+});
+
 ico.mounted(async function (el){
 	el.innerHTML = await (await fetch("https://ico.campani.fr/" + this.slotText + ".svg")).text();
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./components/carousel";
 import "./components/list";
 import "./components/ico";
+import "./components/tab";
 import { getComp } from "./lib/component";
 
 export const currentScript = document.currentScript;
